5dda3f331ab5e65737e25411b7dd08d09b1fefb6
Lucas_Martins <lucaspmartins14@gmail.com>
2024-09-27 21:37:53 -0300

    teste

--- bioeng_maintence/lib/main.dart
+++ bioeng_maintence/lib/main.dart
@@ -1,6 +1,22 @@
+<<<<<<< HEAD
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'firebase_options.dart';
+import 'login_page.dart';
+import 'package:firebase_database/firebase_database.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+=======
 import 'package:flutter/material.dart';
 
 void main() {
+>>>>>>> parent of 55674c1 (configuração banco de dados)
   runApp(const MyApp());
 }
 
@@ -13,6 +29,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+<<<<<<< HEAD
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const LoginPage(),
+=======
         // This is the theme of your application.
         //
         // TRY THIS: Try running your application with "flutter run". You'll see
@@ -32,12 +54,15 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
+>>>>>>> parent of 55674c1 (configuração banco de dados)
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
+<<<<<<< HEAD
+=======
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -48,6 +73,7 @@ class MyHomePage extends StatefulWidget {
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
 
+>>>>>>> parent of 55674c1 (configuração banco de dados)
   final String title;
 
   @override
@@ -56,6 +82,66 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+<<<<<<< HEAD
+  Map<String, dynamic>? _userData;
+  String ticket = 'Não validado'; // Variável inicializada.
+
+  final DatabaseReference _databaseReference = FirebaseDatabase.instance.ref();
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUserData();
+  }
+
+  void _loadUserData() {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      DatabaseReference userRef =
+          _databaseReference.child('users/medicos').child(user.uid);
+      userRef.once().then((DatabaseEvent event) {
+        if (event.snapshot.exists) {
+          setState(() {
+            _userData = Map<String, dynamic>.from(event.snapshot.value as Map);
+            _userData!['userType'] = 'Médico';
+            _counter = _userData!['counter'] ?? 0;
+          });
+        } else {
+          userRef = _databaseReference.child('users/pacientes').child(user.uid);
+          userRef.once().then((DatabaseEvent event) {
+            if (event.snapshot.exists) {
+              setState(() {
+                _userData =
+                    Map<String, dynamic>.from(event.snapshot.value as Map);
+                _userData!['userType'] = 'Paciente';
+                _counter = _userData!['counter'] ?? 0;
+              });
+            }
+          });
+        }
+      });
+    }
+  }
+
+  void _incrementCounter() {
+    if (_userData != null) {
+      setState(() {
+        _counter++;
+        User? user = FirebaseAuth.instance.currentUser;
+        if (user != null) {
+          String userType =
+              _userData!['userType'] == 'Médico' ? 'medicos' : 'pacientes';
+          _databaseReference
+              .child('users')
+              .child(userType)
+              .child(user.uid)
+              .update({
+            'counter': _counter,
+          });
+        }
+      });
+    }
+=======
 
   void _incrementCounter() {
     setState(() {
@@ -66,6 +152,17 @@ class _MyHomePageState extends State<MyHomePage> {
       // called again, and so nothing would appear to happen.
       _counter++;
     });
+>>>>>>> parent of 55674c1 (configuração banco de dados)
+  }
+
+  void readQRCode() async {
+    String code = await FlutterBarcodeScanner.scanBarcode(
+      "FFFFFF",
+      "Cancelar",
+      false,
+      ScanMode.QR,
+    );
+    setState(() => ticket = code != '-1' ? code : 'Não validado');
   }
 
   @override
@@ -77,14 +174,43 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: [
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('Início'),
+              subtitle: const Text('Tela de início'),
+              onTap: () {
+                print('home');
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.logout),
+              title: const Text('Logout'),
+              subtitle: const Text('Finalizar sessão'),
+              onTap: () {
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(builder: (context) => const LoginPage()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
       appBar: AppBar(
         // TRY THIS: Try changing the color here to a specific color (to
         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
         // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+<<<<<<< HEAD
+        title: Text(widget.title),
+        leading: null,
+=======
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+>>>>>>> parent of 55674c1 (configuração banco de dados)
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
@@ -105,6 +231,32 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+<<<<<<< HEAD
+            if (_userData != null) ...[
+              Text('Nome: ${_userData!['name']}',
+                  style: Theme.of(context).textTheme.bodyLarge),
+              Text('E-mail: ${_userData!['email']}',
+                  style: Theme.of(context).textTheme.bodyLarge),
+              Text('Tipo de Usuário: ${_userData!['userType']}',
+                  style: Theme.of(context).textTheme.bodyLarge),
+              if (_userData!['userType'] == 'Médico')
+                Text('CRM: ${_userData!['crm'] ?? 'N/A'}',
+                    style: Theme.of(context).textTheme.bodyLarge),
+              Text('Data de Nascimento: ${_userData!['dateOfBirth']}',
+                  style: Theme.of(context).textTheme.bodyLarge),
+            ] else ...[
+              const Text('Carregando informações do usuário...'),
+            ],
+            const SizedBox(height: 20),
+            const Text('Você pressionou o botão esta quantidade de vezes:'),
+            Text('$_counter',
+                style: Theme.of(context).textTheme.headlineMedium),
+            const SizedBox(height: 20),
+            Text('Código do QR: $ticket',
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyLarge), // Exibe o código do QR
+=======
             const Text(
               'You have pushed the button this many times:',
             ),
@@ -112,14 +264,22 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+>>>>>>> parent of 55674c1 (configuração banco de dados)
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
+<<<<<<< HEAD
+        onPressed: readQRCode,
+        tooltip: 'QR Code',
+        child: const Icon(Icons.qr_code),
+      ),
+=======
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+>>>>>>> parent of 55674c1 (configuração banco de dados)
     );
   }
 }
